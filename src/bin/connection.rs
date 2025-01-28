2a53139fb9fdbb82df759a5bda3797336560eefe
hasmokan <1021056159@qq.com>
2025-01-28 12:45:12 +0800

    feat: use env var

---
+++ src/bin/connection.rs
@@ -0,0 +1,115 @@
+use std::io::Cursor;
+
+use bytes::Buf;
+use bytes::BytesMut;
+use mini_redis::frame::Error::Incomplete;
+use mini_redis::{Frame, Result};
+use tokio::io::{self, AsyncWriteExt};
+use tokio::{
+    io::{AsyncReadExt, BufWriter},
+    net::TcpStream,
+};
+
+pub struct Connection {
+    stream: BufWriter<TcpStream>,
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream: BufWriter::new(stream),
+            buffer: BytesMut::with_capacity(4096),
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> crate::Result<Option<Frame>> {
+        loop {
+            // Attempt to parse a frame from the buffered data. If enough data
+            // has been buffered, the frame is returned.
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            // There is not enough buffered data to read a frame. Attempt to
+            // read more data from the socket.
+            //
+            // On success, the number of bytes is returned. `0` indicates "end
+            // of stream".
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                // The remote closed the connection. For this to be a clean
+                // shutdown, there should be no data in the read buffer. If
+                // there is, this means that the peer closed the socket while
+                // sending a frame.
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
+    }
+
+    pub fn parse_frame(&mut self) -> Result<Option<Frame>> {
+        let mut buf = Cursor::new(&self.buffer[..]);
+
+        // 检查是否读取了足够解析出一个帧的数据
+        match Frame::check(&mut buf) {
+            Ok(_) => {
+                // 获取组成该帧的字节数
+                let len = buf.position() as usize;
+
+                // 在解析开始之前，重置内部的游标位置
+                buf.set_position(0);
+
+                // 解析帧
+                let frame = Frame::parse(&mut buf)?;
+
+                // 解析完成，将缓冲区该帧的数据移除
+                self.buffer.advance(len);
+
+                // 返回解析出的帧
+                Ok(Some(frame))
+            }
+            // 缓冲区的数据不足以解析出一个完整的帧
+            Err(Incomplete) => Ok(None),
+            // 遇到一个错误
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    async fn write_frame(&mut self, frame: &Frame) -> io::Result<()> {
+        match frame {
+            Frame::Simple(val) => {
+                self.stream.write_u8(b'+').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Error(val) => {
+                self.stream.write_u8(b'-').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Integer(val) => {
+                self.stream.write_u8(b':').await?;
+                self.write_decimal(*val).await?;
+            }
+            Frame::Null => {
+                self.stream.write_all(b"$-1\r\n").await?;
+            }
+            Frame::Bulk(val) => {
+                let len = val.len();
+
+                self.stream.write_u8(b'$').await?;
+                self.write_decimal(len as u64).await?;
+                self.stream.write_all(val).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Array(_val) => unimplemented!(),
+        }
+
+        self.stream.flush().await;
+
+        Ok(())
+    }
+}
